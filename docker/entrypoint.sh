1ba4e9c420190dc295e622126ac8164e61a341dc
1KELER1 <123807688+1KELER1@users.noreply.github.com>
2025-04-11 21:40:12 +0800

    fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,19 +7,27 @@ set -o nounset
 postgres_ready() {
     python << END
 import sys
-
+import os
 from psycopg2 import connect
 from psycopg2.errors import OperationalError
 
 try:
+    print(f"Trying to connect to PostgreSQL with:")
+    print(f"Host: {os.getenv('POSTGRES_HOST')}")
+    print(f"Port: {os.getenv('POSTGRES_PORT')}")
+    print(f"Database: {os.getenv('POSTGRES_DATABASE')}")
+    print(f"User: {os.getenv('POSTGRES_USER')}")
+    
     connect(
-        dbname="${POSTGRES_DATABASE}",
-        user="${POSTGRES_USER}",
-        password="${POSTGRES_PASSWORD}",
-        host="${POSTGRES_HOST}",
-        port="${POSTGRES_PORT}",
+        dbname=os.getenv('POSTGRES_DATABASE'),
+        user=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        host=os.getenv('POSTGRES_HOST'),
+        port=os.getenv('POSTGRES_PORT'),
     )
-except OperationalError:
+    print("Successfully connected to PostgreSQL!")
+except OperationalError as e:
+    print(f"Error connecting to PostgreSQL: {e}")
     sys.exit(-1)
 END
 }
